f0de63d61a2f9651e1475f2f03be2a238ee9c57b
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-11-21 11:22:08 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿Main();
+
+
+
+void Main()
+{
+    int number1 = 1;
+    int number2 = 5;
+
+    var sum = ReturnNumbers(number1, number2);
+
+    Console.WriteLine(sum);
+    Console.WriteLine("denne metoden returnerer ingen ting");
+}
+
+int ReturnNumbers(int number1, int number2)
+{
+    var number3 = number1 + number2;
+    return number3;
+}
